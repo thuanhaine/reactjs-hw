73203b54b125fd92c4e83dbac44f1c5b41c7ee34
thuanhaine <19521462@gm.uit.edu.vn>
2022-11-08 21:56:10 +0700

    fetch api

--- src/component/product/page1/Page1.jsx
+++ src/component/product/page1/Page1.jsx
@@ -1,68 +1,36 @@
 import ImgProduct from '../../img/girl.jpg'
 import './Page1.css'
+import { useState, useEffect } from 'react'
+
 
 function Page1() {
+    const [books, setBooks] = useState([])
+
+    useEffect(() => {
+        fetch('https://resfulapitest.herokuapp.com/api/get')
+        .then(res => res.json())
+        .then( data => {
+            console.log(data);
+            setBooks(data)
+        })
+    }, [])
+    
     return(
         <>
         <ul className='product-list'>
-        <li className="product-item">
-                 <img className='product-img' src={ImgProduct} alt="product img"/>
-                 <div className='product-info'>
-                    <p className='product-name'>Truyện Kiều</p>
-                    <p className='product-author'>Nguyễn Du</p>
-                    <p className='product-price'>100.000đ</p>
-                 </div>
-                 <button className='product-btn-buy'>Buy</button>
+            {books.map(book => (
+                <li className="product-item" key={book.id}>
+                <img className='product-img' src={ImgProduct} alt="product img"/>
+                <div className='product-info'>
+                   <p className='product-name'>{book.name}</p>
+                   <p className='product-author'>{book.author || "Đang cập nhật ..."}</p>
+                   <p className='product-price'>{book.price}</p>
+                </div>
+                <button className='product-btn-buy'>Buy</button>
 
-            </li>
-            <li className="product-item">
-                 <img className='product-img' src={ImgProduct} alt="product img" />
-                 <div className='product-info'>
-                    <p className='product-name'>Truyện Kiều</p>
-                    <p className='product-author'>Nguyễn Du</p>
-                    <p className='product-price'>100.000đ</p>
-                 </div>
-                 <button className='product-btn-buy'>Buy</button>
+           </li>
 
-            </li>
-            <li className="product-item">
-                 <img className='product-img' src={ImgProduct} alt="product img" />
-                 <div className='product-info'>
-                    <p className='product-name'>Truyện Kiều</p>
-                    <p className='product-author'>Nguyễn Du</p>
-                    <p className='product-price'>100.000đ</p>
-                 </div>
-                 <button className='product-btn-buy'>Buy</button>
-
-            </li>
-            <li className="product-item">
-                 <img className='product-img' src={ImgProduct} alt="product img" />
-                 <div className='product-info'>
-                    <p className='product-name'>Truyện Kiều</p>
-                    <p className='product-author'>Nguyễn Du</p>
-                    <p className='product-price'>100.000đ</p>
-                 </div>
-                 <button className='product-btn-buy'>Buy</button>
-            </li>
-            <li className="product-item">
-                 <img className='product-img' src={ImgProduct} alt="product img" />
-                 <div className='product-info'>
-                    <p className='product-name'>Truyện Kiều</p>
-                    <p className='product-author'>Nguyễn Du</p>
-                    <p className='product-price'>100.000đ</p>
-                 </div>
-                 <button className='product-btn-buy'>Buy</button>
-
-            </li>
-            <li className="product-item">
-                 <img className='product-img' src={ImgProduct} alt="product img" />
-                 <div className='product-info'>
-                    <p className='product-name'>Truyện Kiều</p>
-                    <p className='product-author'>Nguyễn Du</p>
-                    <p className='product-price'>100.000đ</p>
-                 </div>
-                 <button className='product-btn-buy'>Buy</button>
-            </li>
+            ))}
             
 
               
