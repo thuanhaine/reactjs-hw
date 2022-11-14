6b9dddca6a2f8c060c9e89bfdf6c669283ac1df6
thuanhaine <19521462@gm.uit.edu.vn>
2022-11-14 22:26:06 +0700

    update function for login and regis

--- src/component/login/Login.jsx
+++ src/component/login/Login.jsx
@@ -1,48 +1,93 @@
-import { NavLink } from 'react-router-dom';
-import './Login.css';
+import { NavLink, useHistory } from "react-router-dom";
+import "./Login.css";
+import { useState } from "react";
+import { Redirect} from 'react-router-dom'
 
-
-
-
+let dataUser = {
+  name: String,
+  password: String,
+};
 
 function Login() {
-    return (
-        <>
-            <div className="login-container">
-                <div className="login-content">
-                    <h1 className='login-title'>Login</h1>
-                    <div className='login-input-form'>
-                        <span className='login-input-span'>Username</span>
-                        <input type="text" className='login-input-box' placeholder='Nhap username'></input>
-                    </div>
-                    <div className='login-input-form'>
-                        <span className='login-input-span'>Password</span>
-                        <input type="password" className='login-input-box' placeholder='Nhap Password'></input>
-                    </div>
-                        <button className="btn-login mr-32">
-                            <NavLink className="btn__login-route" to="/home" onClick={this}>Login</NavLink>
-                        </button>
-                    <div className='text__forgot mr-16'>
-                    <span >Forgot Password ?</span>
+  const [inputName, setName] = useState("");
+  const [inputPassword, setPassword] = useState("");
+    const history = useHistory();
+  const handleLogin = (e) => {
+    e.preventDefault();
+    dataUser = {
+      name: inputName,
+      password: inputPassword,
+    };
+    console.log(dataUser);
 
-                    </div>
-                    <div className='login__register-link mr-16'>
-                        <span className=''>
-                            Don't have account ? click <NavLink className="navLink" to = "/signup" onClick={this}><strong>Here</strong></NavLink>
-                        </span>
-                    </div>
-                    <div className='login__more'>
-                        <span className='login__more-text'>Login More</span>
-                        <div className='login__more-icon '>
-                             <i class="fa fa-google google"></i>
-                             <i class="fa fa-facebook facebook"></i>
-                             <i class="fa fa-twitter twitter"></i>
-                        </div>
-                    </div>
-                </div>
+    fetch("http://localhost:3002/api/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(dataUser),
+    })
+      .then((data) => data.json())
+      .then((data) => {
+        if(data.status == 200){
+            alert("SuccessLogin")
+            history.replace('/')
+    
+    }
+        else alert("sai")
+      })
+      .catch((err) => console.log(err.data));
+  };
+
+  return (
+    <>
+      <form onSubmit={handleLogin}>
+        <div className="login-container">
+          <div className="login-content">
+            <h1 className="login-title">Login</h1>
+            <div className="login-input-form">
+              <span className="login-input-span">Username</span>
+              <input
+                type="text"
+                className="login-input-box"
+                placeholder="Nhap username"
+                onChange={e => setName(e.target.value)}
+              ></input>
             </div>
-        </>
-    )
+            <div className="login-input-form">
+              <span className="login-input-span">Password</span>
+              <input
+                type="password"
+                className="login-input-box"
+                placeholder="Nhap Password"
+                onChange={ e => setPassword(e.target.value)}
+              ></input>
+            </div>
+            <button className="btn-login mr-32">Login</button>
+            <div className="text__forgot mr-16">
+              <span>Forgot Password ?</span>
+            </div>
+            <div className="login__register-link mr-16">
+              <span className="">
+                Don't have account ? click{" "}
+                <NavLink className="navLink" to="/signup" onClick={this}>
+                  <strong>Here</strong>
+                </NavLink>
+              </span>
+            </div>
+            <div className="login__more">
+              <span className="login__more-text">Login More</span>
+              <div className="login__more-icon ">
+                <i class="fa fa-google google"></i>
+                <i class="fa fa-facebook facebook"></i>
+                <i class="fa fa-twitter twitter"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
+    </>
+  );
 }
 
 export default Login;
--- src/component/signup/Signup.jsx
+++ src/component/signup/Signup.jsx
@@ -1,41 +1,103 @@
-import './Signup.css';
-import { NavLink } from 'react-router-dom';
+import "./Signup.css";
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
+
+let dataUser = {
+  name: String,
+  password: String,
+  email: String,
+};
+
+function Signup() {
+  const [inputName, setName] = useState("");
+  const [inputPassword, setPassword] = useState("");
+  const [inputEmail, setEmail] = useState("");
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    dataUser = {
+      name: inputName,
+      password: inputPassword,
+      email: inputEmail,
+    };
+    console.log(dataUser);
+
+  fetch('http://localhost:3002/api/signup', {
+    method:  'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(dataUser)
+  })
+  .then(data => data.json())
+  .then(data => {
+    alert("Signup successful")
+  })
+  .catch(err => console.log(err.data))
 
 
 
+  };
 
-
-function signup() {
-    return (
-        <>
-            <div className="signup-container">
-                <div className="signup-content">
-                    <h1 className='signup-title'>Sign Up</h1>
-                    <div className='signup-input-form'>
-                        <span className='signup-input-span'>Username</span>
-                        <input type="text" className='signup-input-box' placeholder='Enter Username'></input>
-                    </div>
-                    
-                    <div className='signup-input-form'>
-                        <span className='signup-input-span'>Password</span>
-                        <input type="password" className='signup-input-box' placeholder='Enter Password'></input>
-                    </div>
-                    <div className='signup-input-form'>
-                        <span className='signup-input-span'>Email</span>
-                        <input type="text" className='signup-input-box' placeholder='Enter your email'></input>
-                    </div>
-                         <button className="btn-signup mr-32">
-                            <NavLink className="btn__singup-route" to="/home" onClick={this}>Sign Up</NavLink>
-                        </button>
-                    <div className='signup__login-link'>
-                      <span>
-                        You already have account <NavLink className="NavLink__to-login" to = "/login" onClick={this}><strong>login</strong></NavLink>
-                      </span>
-                    </div>
-                </div>
+  return (
+    <>
+      <form onSubmit={handleSignup}>
+        <div className="signup-container">
+          <div className="signup-content">
+            <h1 className="signup-title">Sign Up</h1>
+            <div className="signup-input-form">
+              <span className="signup-input-span">Username</span>
+              <input
+                type="text"
+                className="signup-input-box"
+                placeholder="Enter Username"
+                value={inputName}
+                onChange={(e) => setName(e.target.value)}
+              ></input>
             </div>
-        </>
-    )
+
+            <div className="signup-input-form">
+              <span className="signup-input-span">Password</span>
+              <input
+              required
+                type="password"
+                className="signup-input-box"
+                placeholder="Enter Password"
+                value={inputPassword}
+                onChange={(e) => setPassword(e.target.value)}
+              ></input>
+            </div>
+            <div className="signup-input-form">
+              <span className="signup-input-span">Email</span>
+              <input
+                type="email"
+                required
+                className="signup-input-box"
+                placeholder="Enter your email"
+                value={inputEmail}
+                onChange={(e) => setEmail(e.target.value)}
+              ></input>
+            </div>
+            <button className="btn-signup mr-32" >
+                Sign Up
+            </button>
+            <div className="signup__login-link">
+              <span>
+                You already have account{" "}
+                <NavLink
+                  className="NavLink__to-login"
+                  to="/login"
+                  onClick={this}
+                >
+                  <strong>login</strong>
+                </NavLink>
+              </span>
+            </div>
+          </div>
+        </div>
+      </form>
+    </>
+  );
 }
 
-export default signup;
+export default Signup;
